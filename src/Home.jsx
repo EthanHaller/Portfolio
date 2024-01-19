d8c8e5a0920e293a3e2a2f8179ff81d27b3650d3
EthanHaller <ttk4ey@virginia.edu>
2024-01-19 16:23:22 -0500

    feature: create homepage and style

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
+import avatar from "./images/avatar.png"
+import "./styles/home.css"
 
 function Home() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
-			<h1>Hello</h1>
+			<div className="home-block">
+				<div className="home-text">
+					<p className="hello-text">Hello, my name is </p>
+					<h1 className="name">Ethan Haller</h1>
+					<p className="motto-text">
+						I am a versatile software engineer specializing in full stack web applications, with a passion for self-improvement. Currently, I do
+						BLANK at <a href="https://www.yext.com/">Yext</a>.
+					</p>
+				</div>
+				<div className="home-picture">
+					<img src={avatar} alt="avatar" className="avatar"></img>
+				</div>
+			</div>
 		</>
 	)
 }
