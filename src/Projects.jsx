dac393cedfdca4420cf3d4e67583dcc3b89737ed
EthanHaller <ttk4ey@virginia.edu>
2024-02-02 13:28:03 -0500

    style: begin styling other projects and add responsive layout to featured projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,20 +5,21 @@ import "./styles/projects.css"
 
 function Projects({ innerRef }) {
 	const featuredProjects = projectData.filter((project) => project.featured)
+	const otherProjects = projectData.filter((project) => !project.featured)
 
 	const featuredProjectsElement = featuredProjects.map((project, index) => (
 		<div key={index} className={"featured-project-container" + (index % 2 === 1 ? " reverse" : "")}>
 			<div className="featured-project-information">
 				<h3 className={"featured-project-title" + (index % 2 === 1 ? " reverse" : "")}>{project.title}</h3>
 				<ul className={"featured-project-links" + (index % 2 === 1 ? " reverse" : "")}>
-					<a target="_blank" href={project.githubLink} className="featured-project-link">
-						<FaGithub />
-					</a>
 					{project.deploymentLink && (
 						<a target="_blank" href={project.deploymentLink} className="featured-project-link">
 							<LuExternalLink />
 						</a>
 					)}
+					<a target="_blank" href={project.githubLink} className="featured-project-link">
+						<FaGithub />
+					</a>
 				</ul>
 				<p className="featured-project-description">{project.description}</p>
 				<ul className={"skills-list projects" + (index % 2 === 1 ? " reverse" : "")}>
@@ -36,6 +37,28 @@ function Projects({ innerRef }) {
 		</div>
 	))
 
+	const otherProjectsElement = otherProjects.map((project, index) => (
+		<div key={index} className={"other-project-container" + (project.large ? " large" : "")}>
+			<div className={"card-left" + (project.large ? " large" : "")}>
+				<div className="card-header">
+					<h4 className="project-title">{project.title}</h4>
+					{project.deploymentLink && (
+						<a target="_blank" href={project.deploymentLink} className="project-link">
+							<LuExternalLink />
+						</a>
+					)}
+					<a target="_blank" href={project.githubLink} className="project-link">
+						<FaGithub />
+					</a>
+				</div>
+				<p className="project-description">{project.description}</p>
+			</div>
+			<div className={"card-right" + (project.large ? " large" : "")}>
+				<img className={"project-image" + (project.large ? " large" : "")} src={project.imagePath}></img>
+			</div>
+		</div>
+	))
+
 	return (
 		<>
 			<div className="container" ref={innerRef}>
@@ -45,7 +68,14 @@ function Projects({ innerRef }) {
 					<span className="title-line second"></span>
 				</div>
 				<div className="featured-projects-container">{featuredProjectsElement}</div>
-				<div className="other-projects-container"></div>
+				<div className="other-projects-section-title">
+					<span className="title-line first"></span>
+					<h2 className="other-projects-title">Other Projects</h2>
+					<span className="title-line second"></span>
+				</div>
+				<div className="other-projects-container">
+					<div className="grid">{otherProjectsElement}</div>
+				</div>
 			</div>
 		</>
 	)
